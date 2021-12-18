3aa60e311a42bdad1e19aabfd0207a7c445364a4
Shreyas Gokhale <shreyas.gokhale@gropyus.com>
2021-12-18 17:25:17 +0100

    Site overhaul

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,33 +9,10 @@ module.exports = {
       whitelist: [],
     },
   },
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
     },
-    extend: {
-      colors: {},
-      screens: {
-        'dark': { 'raw': '(prefers-color-scheme: dark)' },
-        // => @media (prefers-color-scheme: dark) { ... }
-      }
-    },
-  }, variants: {
-    textColor: ['dark', 'responsive', 'hover', 'focus'],
-    backgroundColor: ['dark', 'responsive', 'hover', 'focus']
-  },
-  plugins: [
-    plugin(function ({ addVariant, prefix }) {
-      addVariant('dark', ({ modifySelectors, separator }) => {
-        modifySelectors(({ selector }) => {
-          return selectorParser((selectors) => {
-            selectors.walkClasses((sel) => {
-              sel.value = `dark${separator}${sel.value}`
-              sel.parent.insertBefore(sel, selectorParser().astSync(prefix('.scheme-dark ')))
-            })
-          }).processSync(selector)
-        })
-      })
-    })
-  ]
+  }
 }
